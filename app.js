b6089066c372dcbbcc7a5b556e38f76cc8a9bb9e
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-08-29 14:06:28 +0200

    refactor class and design pattern

--- app.js
+++ app.js
@@ -10,77 +10,113 @@ class Project {
    }
 }
 
-const projects = [
-   new Project(1, 'Booki', 'oc_projet_2_booki'),
-   new Project(2, 'OhMyFood', 'oc_projet_3_ohmyfood'),
-   new Project(3, 'GameOn', 'oc_projet_4_gameon/starterOnly'),
-   new Project(4, 'FishEye', 'oc_projet_6_fisheye'),
-   new Project(5, 'Pomodoro', 'pomodoro'),
-   new Project(5, 'CDM2022', 'CDM2022'),
-   new Project(6, 'Sestini-Pizza', 'http://sestini-pizza.fr'),
-   new Project(7, 'portfolio', 'https://maxii.me'),
-]
+class Api {
+   url
 
-const links = document.querySelector('#links')
+   constructor(url) {
+      this.url = url
+   }
 
-projects.forEach(project => {
-   const list = document.createElement('li')
-   const link = document.createElement('a')
-   project._label.split('').forEach(l => {
-      const letter = document.createElement('span')
-      letter.className = 'letter'
-      letter.innerText = l.toUpperCase()
-      letter.addEventListener('mouseover', () => {
-         letter.animate(
-            [
-               { transform: 'rotate(0deg)' },
-               { transform: 'rotate(45deg)' },
-               { transform: 'rotate(-45deg)' },
-               { transform: 'rotate(35deg)' },
-               { transform: 'rotate(-32deg)' },
-               { transform: 'rotate(20deg)' },
-               { transform: 'rotate(-15deg)' },
-               { transform: 'rotate(0deg)' },
-            ],
-            1000
-         )
-      })
-      link.appendChild(letter)
-   })
-   link.href = project._url
-   link.className = 'navigation'
-   list.appendChild(link)
-   links.appendChild(list)
-})
-
-// Keyboard Arrow Up | Down Focus Navigation
-const keyUp = document.querySelector('.keys__icon--up')
-const keyDown = document.querySelector('.keys__icon--down')
-
-document.addEventListener('keydown', k => {
-   const links = [...document.querySelectorAll('.navigation')]
-   let count = document.querySelector('#count')
-
-   if (k.key === 'ArrowUp') {
-      keyUp.dataset.keyPress = 'true'
-      if (count.dataset.value === 'null') {
-         count.dataset.value = '0'
-      } else {
-         count.dataset.value === '0' ? (count.dataset.value = '2') : count.dataset.value--
+   async get() {
+      try {
+         const api = fetch(this.url)
+         this.promise = await api.json()
+         return await this.promise
+      } catch (e) {
+         throw new Error('Cannot read data from api...')
       }
-   } else if (k.key === 'ArrowDown') {
-      keyDown.dataset.keyPress = 'true'
-      count.dataset.value < 2 ? count.dataset.value++ : (count.dataset.value = '0')
    }
-   // links[count.dataset.value].focus()
-})
+}
 
-document.addEventListener('keyup', k => {
-   let delay = 150
-   if (k.key === 'ArrowUp') {
-      setTimeout(() => (keyUp.dataset.keyPress = 'false'), delay)
-   } else if (k.key === 'ArrowDown') {
-      setTimeout(() => (keyDown.dataset.keyPress = 'false'), delay)
+class App {
+   constructor() {
+      this.links = document.querySelector('#links')
+      this.api = new Api('sites.json')
    }
-   clearTimeout()
-})
+
+   async getData() {
+      return await this.api.get()
+   }
+
+   getSites(data) {
+      return data.map(site => {
+         new Project(site.id, site.name, site.url)
+      })
+   }
+
+   async renderSites(sites) {
+      sites.forEach(project => {
+         const list = document.createElement('li')
+         const link = document.createElement('a')
+         project._label.split('').forEach(l => {
+            const letter = document.createElement('span')
+            letter.className = 'letter'
+            letter.innerText = l.toUpperCase()
+            letter.addEventListener('mouseover', () => {
+               letter.animate(
+                  [
+                     { transform: 'rotate(0deg)' },
+                     { transform: 'rotate(45deg)' },
+                     { transform: 'rotate(-45deg)' },
+                     { transform: 'rotate(35deg)' },
+                     { transform: 'rotate(-32deg)' },
+                     { transform: 'rotate(20deg)' },
+                     { transform: 'rotate(-15deg)' },
+                     { transform: 'rotate(0deg)' },
+                  ],
+                  1000
+               )
+            })
+            link.appendChild(letter)
+         })
+         link.href = project._url
+         link.className = 'navigation'
+         list.appendChild(link)
+         this.links.appendChild(list)
+      })
+   }
+
+   keyBoardNavigation() {
+      // Keyboard Arrow Up | Down Focus Navigation
+      const keyUp = document.querySelector('.keys__icon--up')
+      const keyDown = document.querySelector('.keys__icon--down')
+
+      document.addEventListener('keydown', k => {
+         const links = [...document.querySelectorAll('.navigation')]
+         let count = document.querySelector('#count')
+
+         if (k.key === 'ArrowUp') {
+            keyUp.dataset.keyPress = 'true'
+            if (count.dataset.value === 'null') {
+               count.dataset.value = '0'
+            } else {
+               count.dataset.value === '0' ? (count.dataset.value = '2') : count.dataset.value--
+            }
+         } else if (k.key === 'ArrowDown') {
+            keyDown.dataset.keyPress = 'true'
+            count.dataset.value < 2 ? count.dataset.value++ : (count.dataset.value = '0')
+         }
+         // links[count.dataset.value].focus()
+      })
+
+      document.addEventListener('keyup', k => {
+         let delay = 150
+         if (k.key === 'ArrowUp') {
+            setTimeout(() => (keyUp.dataset.keyPress = 'false'), delay)
+         } else if (k.key === 'ArrowDown') {
+            setTimeout(() => (keyDown.dataset.keyPress = 'false'), delay)
+         }
+         clearTimeout()
+      })
+   }
+
+   async init() {
+      const data = await this.getData()
+      const projects = await this.getSites(data)
+      await this.renderSites(projects)
+      this.keyBoardNavigation()
+   }
+}
+
+const app = new App()
+app.init()
